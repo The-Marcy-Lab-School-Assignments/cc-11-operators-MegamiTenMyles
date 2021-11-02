b903d49c6f18f5699617927bf10a4556c6bc6ba8
MegamiTenMyles <mylesmatthews2112@gmail.com>
2021-11-02 14:37:48 +0000

    questions answered

--- index.js
+++ index.js
@@ -1,27 +1,58 @@
 //1
-function areBothSame(){
-  
+function areBothSame(x, y){
+  //Put two parameters that can be input.
+  //Write If statement to check if the parameter is true
+  if (x == y) {
+      return true
+      //checks if x is equal to y and if it is returns true
+  } else {
+      return false
+      // if the equal fails it will return false
+  }
 }
-
+// console.log(areBothSame(true, 1))
 //2
-function areBothEqual(){
+function areBothEqual(x, y){
+      //Put two parameters that can be input.
+  //Write If statement to check if the parameter is true
+  if (x == y) {
+      return true
+      //checks if x is equal to y and if it is returns true
+  } else {
+      return false
+      // if the equal fails it will return false
+  }
   
 }
 
 //3
-function areAllFourEqual(){
-  
+function areAllFourEqual(x, y, z , v){
+  if (x == y && x == z && x == v ) {
+      return true
+  } else {
+      return false
+  }
 }
-
+//console.log(areAllFourEqual(2, 2, "2", "2"))
 
 //4
-function areAllFourSame(){
-  
+function areAllFourSame(x, y, z, v){
+   if (x === y && x === z && x === v ) {
+      return true
+  } else {
+      return false
+  }
 }
 
 //5
-function areAllFourSameOrEqual(){
-  
+function areAllFourSameOrEqual(x , y, z, v){
+  if (x == y && x == z && x == v ) {
+      return "same"
+  } else if (x === y && x === z && x === v) {
+      return "equal"
+  } else {
+      return null
+  }
 }
 
 
